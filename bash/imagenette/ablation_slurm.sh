2605183a4340a6b23487c749a64d76286d591bb9
jaryP <jarypomponi@gmail.com>
2024-10-25 17:48:47 +0200

    Ablations added

--- bash/imagenette/ablation_slurm.sh
+++ bash/imagenette/ablation_slurm.sh
@@ -1,18 +1,3 @@
-#!/usr/bin/env bash
-
-METHOD=$1
-DEVICE=$2
-
-case $METHOD in
-adaptive)
-  python python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 +model=imagenette224_vit16 method=proposal device="$DEVICE"
-  python python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal device="$DEVICE"
-  ;;
-*)
-  echo -n "Unrecognized method"
-esac
-
-
 #!/bin/sh
 #SBATCH -A IscrC_BEVITIN
 #SBATCH -p boost_usr_prod

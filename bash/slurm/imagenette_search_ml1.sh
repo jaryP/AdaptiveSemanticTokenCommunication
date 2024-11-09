ed68550d86962098ddbc14013def75ec375c5548
jaryP <jarypomponi@gmail.com>
2024-11-09 13:13:56 +0100

    Slurm modified

--- bash/slurm/imagenette_search_ml1.sh
+++ bash/slurm/imagenette_search_ml1.sh
@@ -21,6 +21,14 @@ source activate eep
 
 
 #srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=0.1 device=0
-srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=0.5  final_evaluation=semantic device=0
-srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=1 final_evaluation=semantic device=0
-srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=1.5 final_evaluation=semantic device=0
+#srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=0.5  final_evaluation=semantic device=0
+#srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=1 final_evaluation=semantic device=0
+#srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=1.5 final_evaluation=semantic device=0
+
+for inner_w in 0 0.1 0.5 1 1.5 2
+do
+  for out_w in 0.1 0.5 1 1.5 2
+  do
+    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=ml1 method.loss.inner_flops_w=$inner_w method.loss.output_flops_w=$out_w  final_evaluation=semantic device=0
+  done
+done

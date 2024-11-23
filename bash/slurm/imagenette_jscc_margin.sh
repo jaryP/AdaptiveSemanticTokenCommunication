42fda00eb3ed7491a85003caf1e79e8945626418
jaryP <jarypomponi@gmail.com>
2024-11-23 16:41:10 +0100

    Slurm file modified

--- bash/slurm/imagenette_jscc_margin.sh
+++ bash/slurm/imagenette_jscc_margin.sh
@@ -22,14 +22,14 @@ for inner_w in 0.5 1
   do
   for out_w in 0.5 1 2
   do
-    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=6 serialization.values_to_prepend=[jscc] device=0
+    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=6 +jscc=proposal +comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
   done
 done
 
-for inner_w in 0.5 1
-  do
-  for out_w in 0.5 1 2
-  do
-    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +jscc=proposal +comm_evaluation=semantic +method.model.blocks_to_transform=6 serialization.values_to_prepend=[jscc] device=0
-  done
-done
+#for inner_w in 0.5 1
+#  do
+#  for out_w in 0.5 1 2
+#  do
+#    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +jscc=proposal +comm_evaluation=semantic +method.model.blocks_to_transform=6 serialization.values_to_prepend=[jscc] device=0
+#  done
+#done

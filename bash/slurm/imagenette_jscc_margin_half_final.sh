a3432e8edd927a63dc65f42fe9aa57bc5d2315c5
jaryP <jarypomponi@gmail.com>
2024-12-30 01:50:04 +0100

    Final training file added

--- bash/slurm/imagenette_jscc_margin_half_final.sh
+++ bash/slurm/imagenette_jscc_margin_half_final.sh
@@ -22,7 +22,7 @@ source activate eep
 
 for inner_w in 1
   do
-  for out_w in 5
+  for out_w in 2
   do
     srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=3 +jscc=proposal_half comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
   done

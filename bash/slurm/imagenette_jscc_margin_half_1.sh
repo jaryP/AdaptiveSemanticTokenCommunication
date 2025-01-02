d4a3283287d1db293aa74b0381a0fdd83bf7d0e9
jaryP <jarypomponi@gmail.com>
2025-01-02 12:39:32 +0100

    Slurm files modified

--- bash/slurm/imagenette_jscc_margin_half_1.sh
+++ bash/slurm/imagenette_jscc_margin_half_1.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# https://stackoverflow.com/questions/27708656/pass-command-line-arguments-via-sbatch
 sbatch <<EOT
 #!/bin/sh
 #SBATCH -A IscrC_BEVITIN
@@ -8,7 +9,7 @@ sbatch <<EOT
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --job-name=6g_adaptive_half
+#SBATCH --job-name="6g_adaptive_half_"$1"_"$2"
 #SBATCH --out="./sout/6g_adaptive_half_"$1"_"$2".out"
 #SBATCH --open-mode=truncate
 
@@ -33,15 +34,7 @@ job_name=""
 #  do
 #  for out_w in 1 0.5 2 5
 #  do
-srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=3 comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
+srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=${inner_w}  method.loss.output_flops_w=${out_w}  final_evaluation=semantic +method.model.blocks_to_transform=3 comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
 #  done
 #done
 EOT
-
-#for inner_w in 0.5 1
-#  do
-#  for out_w in 0.5 1 2
-#  do
-#    srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +jscc=proposal_half +comm_evaluation=semantic +method.model.blocks_to_transform=6 serialization.values_to_prepend=[jscc] device=0
-#  done
-#done

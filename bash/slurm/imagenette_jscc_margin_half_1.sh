86569368320a51a24accac62b07e43cbd0c87002
jaryP <jarypomponi@gmail.com>
2025-01-02 12:07:12 +0100

    Slurm files modified

--- bash/slurm/imagenette_jscc_margin_half_1.sh
+++ bash/slurm/imagenette_jscc_margin_half_1.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+sbatch <<EOT
+#!/bin/sh
 #SBATCH -A IscrC_BEVITIN
 #SBATCH -p boost_usr_prod
 #SBATCH --time=24:00:00
@@ -6,7 +8,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --out=./sout/adaptive_jsccn_margin_half_1.out
+#SBATCH --job-name=6g_adaptive_half
+#SBATCH --out="./sout/6g_adaptive_half_"${inner_w}"_"${out_w}".out"
 #SBATCH --open-mode=truncate
 
 echo "NODELIST="${SLURM_NODELIST}
@@ -25,14 +28,15 @@ source activate eep
 
 inner_w=$1
 out_w=$2
-job_name="6g_adaptive_half_${inner_w}_${out_w}"
+job_name=""
 #for inner_w in 0.5
 #  do
 #  for out_w in 1 0.5 2 5
 #  do
-srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=3 comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0 -J $job_name
+srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=$inner_w  method.loss.output_flops_w=$out_w  final_evaluation=semantic +method.model.blocks_to_transform=3 comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
 #  done
 #done
+EOT
 
 #for inner_w in 0.5 1
 #  do

8e0c59bfeaa8cbda2686fe680ca4df928774a3b6
jaryP <jarypomponi@gmail.com>
2025-01-05 12:19:25 +0100

    main file modified

--- bash/slurm/imagenette_jscc_margin_half.sh
+++ bash/slurm/imagenette_jscc_margin_half.sh
@@ -28,11 +28,5 @@ conda init
 #conda activate eep
 source activate eep
 
-#for inner_w in 0.5
-#  do
-#  for out_w in 1 0.5 2 5
-#  do
 srun python main.py training_pipeline=imagenette224_vit16 pretraining_pipeline=imagenette224 model=deit_tiny_patch16_224 method=proposal method.loss.inner_flops_type=margin method.loss.inner_flops_w=${1}  method.loss.output_flops_w=${2}  final_evaluation=semantic +method.model.blocks_to_transform=3 comm_evaluation=semantic serialization.values_to_prepend=[jscc] device=0
-#  done
-#done
 EOT

52e828b9a2f98fc67e5407de2044d558d39583df
jaryP <jarypomponi@gmail.com>
2025-01-08 10:27:03 +0100

    Evaluation fixed

--- methods/proposal/evaluation.py
+++ methods/proposal/evaluation.py
@@ -57,7 +57,8 @@ def semantic_evaluation(model: SemanticVit,
                 if b.last_mask.shape[1] < b.num_patches:
                     average_dropping[i].append(b.last_mask.shape[1])
                 else:
-                    average_dropping[i].extend(b.last_mask.mean(1).squeeze(-1).cpu().detach().tolist())
+                    average_dropping[i].extend(torch.count_nonzero(b.last_mask, 1).squeeze(-1).cpu().detach().tolist())
+                    # average_dropping[i].extend(b.last_mask.mean(1).squeeze(-1).cpu().detach().tolist())
 
         accuracy[a] = c / t
 
